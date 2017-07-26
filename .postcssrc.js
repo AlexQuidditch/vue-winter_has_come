fba3093f193649aafa2d00e4de6fd29b9186f9ef
AlexQuidditch <alexquidditch@gmail.com>
2017-07-27 00:30:18 +0300

    initial commit

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+	"plugins": {
+		// to edit target browsers: use "browserlist" field in package.json
+		"autoprefixer": {},
+		"postcss-assets": {},
+		"postcss-nested": {},
+		"postcss-short": {},
+		"postcss-flexbox": {},
+		"postcss-easing-gradients": {},
+		"postcss-google-color": {},
+		"css-mqpacker": {},
+		"rucksack-css": {}
+	}
+}

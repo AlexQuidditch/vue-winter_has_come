ebe31a4a12e33e8bb5c457220a2e86735a062114
AlexQuidditch <alexquidditch@gmail.com>
2017-07-27 00:31:59 +0300

    added layout/main

---
+++ src/components/layout/main.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+	<transition name="fade" mode="out-in" appear>
+		<router-view></router-view>
+	</transition>
+</template>
+
+<script>
+
+    export default {
+        name: "main",
+    }
+
+</script>
+
+<style lang="scss">
+
+    .main {
+    	position: relative;
+    	top: 100px;
+		width: 975px;
+		min-height: calc( 100vh - 100px );
+		padding: 30px;
+		background-color: #f0f4f7;
+		background-color: var(--pale-grey);
+    }
+
+</style>

b7c5167c7fc672618028a575f002a4255cbd51df
AlexQuidditch <alexquidditch@gmail.com>
2017-07-27 00:35:33 +0300

    added views

---
+++ src/components/views/login.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+    <main class="main">
+		<section id="login-page" class="login-page">
+			<h1>{{ 'login page'.toUpperCase() }}</h1>
+			<router-link :to="{ name: 'main-page' }">main-page</router-link>
+	    </section>
+    </main>
+</template>
+
+<script>
+
+    export default {
+        name: "login-page",
+        data() {
+            return {
+
+            }
+        }
+    }
+
+</script>
+
+<style lang="scss">
+
+    .login-page {}
+
+</style>

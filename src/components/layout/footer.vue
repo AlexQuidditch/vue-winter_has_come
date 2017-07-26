0e79f4b6de3684c5b21bab7200bd5223b1a6442b
AlexQuidditch <alexquidditch@gmail.com>
2017-07-27 00:30:54 +0300

    added layout/footer

---
+++ src/components/layout/footer.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <footer class="footer">
+		<h3>Footer</h3>
+    </footer>
+</template>
+
+<script>
+
+    export default {
+        name: "footer",
+        data() {
+            return {
+
+            }
+        }
+    }
+
+</script>
+
+<style lang="scss">
+
+    .footer {}
+
+</style>

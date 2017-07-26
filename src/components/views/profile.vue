3756ac297c8d8f1233261480b8db0a2eb705eff4
AlexQuidditch <alexquidditch@gmail.com>
2017-07-27 00:33:24 +0300

    added views/profile & components

---
+++ src/components/views/profile.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+	<main class="main">
+	</main>
+</template>
+
+<script>
+
+    export default {
+        name: "profile",
+        data() {
+            return {
+				title: 'Hello'
+            }
+        },
+		computed: {
+		    Personal() {
+		        return this.$store.state.User.personal
+		    },
+			Information() {
+				return this.$store.state.User.information
+			}
+		}
+    }
+
+</script>
+
+<style lang="scss">
+
+    .profile {}
+
+</style>

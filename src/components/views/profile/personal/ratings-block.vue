685973bcaecd9d84573faee9f9f216763ae64248
AlexQuidditch <alexquidditch@gmail.com>
2017-08-26 02:51:46 +0300

    profile works

---
+++ src/components/views/profile/personal/ratings-block.vue
@@ -0,0 +1,75 @@
+<template lang="html">
+    <div class="ratings">
+		<h4 class="ratings__main-rate">{{ Ratings.mainRate }}</h4>
+		<star-rating v-model="Ratings.mainRate"
+			:increment="0.1"
+			:star-size="18"
+			:show-rating="false"
+			active-color="#009d2f"
+			inactive-color="rgba(0, 157, 47, 0.35)"
+			read-only>
+		</star-rating>
+		<ul class="ratings-list">
+			<ratings-item :Item="Ratings.average"></ratings-item>
+			<ratings-item :Item="Ratings.completed"></ratings-item>
+			<ratings-item :Item="Ratings.tests"></ratings-item>
+		</ul>
+    </div>
+</template>
+
+<script>
+
+	import StarRating from 'vue-star-rating';
+
+	import ratingsItem from '@templates/rating-item';
+
+    export default {
+        name: "ratings",
+		components: { StarRating , ratingsItem },
+		computed: {
+		    Ratings() {
+		        return this.$store.state.User.ratings
+		    }
+		}
+    }
+
+</script>
+
+<style lang="scss">
+
+	@import "../../../../stylesheets/partials/_mixins";
+
+    .ratings {
+    	display: flex;
+		flex-flow: column;
+		align-items: center;
+		size: 291px 234px;
+		padding: 20px;
+		border-radius: 3px;
+		background-color: #ffffff;
+		background-color: var(--whited);
+		@include MDShadow-1( #4a4a4a );
+		&__main-rate {
+			width: 100%;
+			text-align: center;
+			font-size: 48px;
+			font-weight: 100;
+			color: #4a4a4a;
+			color: var(--charcoal-grey);
+		}
+		> .star-rating {
+			margin: 10px auto;
+			span {
+				margin: 0 4px;
+			}
+		}
+    }
+
+	.ratings-list {
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: space-between;
+		width: 100%;
+	}
+
+</style>
---
+++ src/components/views/profile/social/social-teams.vue
@@ -0,0 +1,52 @@
+<template lang="html">
+    <div class="social-teams">
+		<img src=" ../../../../../static/assets/profile/social/users.svg " alt="Команды"
+				class="social-teams__icon" />
+		<h4 class="social-teams__title">Команды</h4>
+		<p class="social-teams__text">
+			<span v-for="( teamItem , index ) in Teams" :key="index">{{ teamItem }} </span>
+		</p>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "social-teams",
+		computed: {
+		    Teams() {
+		        return this.$store.state.User.social.teams
+		    }
+		}
+    }
+
+</script>
+
+<style lang="scss">
+
+    .social-teams {
+		display: flex;
+		flex-flow: row wrap;
+		align-items: flex-start;
+		width: 410px;
+		&__icon {
+			width: 28px;
+		}
+		&__title {
+			margin-left: 10px;
+			font-size: 18px;
+			line-height: 28px;
+			color: #4a4a4a;
+			color: var(--charcoal-grey);
+		}
+		&__text {
+			margin: 20px 0 0 0;
+			font-size: 13px;
+			font-weight: 300;
+			line-height: 1.46;
+			color: #4a4a4a;
+			color: var(--charcoal-grey);
+		}
+	}
+
+</style>

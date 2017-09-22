2990967fc0c2df92afaa3a89b3388f4a0aacaba0
aleksey.zabaykalsky <aleksey.zabaykalskiy@gotechsoftware.com>
2017-09-23 02:30:35 +0300

    added dateFormat helper

---
+++ src/helpers/dateFormat.js
@@ -0,0 +1,15 @@
+const longDate = {
+  hour: '2-digit',
+  minute: '2-digit',
+  day: '2-digit',
+  month: 'long'
+};
+// "2017-09-20T20:32:25.884Z" --> 20 сентября, 23:32;
+
+const shortDate = {
+  hour: '2-digit',
+  minute: '2-digit'
+};
+// "2017-09-03T18:23:29.078Z" --> 21:23
+
+export default { longDate , shortDate };

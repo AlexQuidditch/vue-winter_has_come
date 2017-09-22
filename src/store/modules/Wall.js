9c1e03018ce5320b4d4eeb59345b7b820c5230a6
aleksey.zabaykalsky <aleksey.zabaykalskiy@gotechsoftware.com>
2017-09-23 02:34:07 +0300

    comments IDs more specificly now

--- src/store/modules/Wall.js
+++ src/store/modules/Wall.js
@@ -33,15 +33,15 @@ const state = [
         time: "2017-09-20T20:33:25.884Z",
         content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         attacments: '',
-        likes: 12
+        likes: 87
       },
       {
-        id: 5,
+        id: 3,
         authorID: 2,
         time: "2017-09-20T20:39:25.884Z",
         content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         attacments: '',
-        likes: 12
+        likes: 45
       }
     ]
   },
@@ -55,7 +55,7 @@ const state = [
     reposts: 1,
     comments: [
       {
-        id: 3,
+        id: 4,
         authorID: 1,
         time: "2017-09-20T20:32:25.884Z",
         content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -74,12 +74,12 @@ const state = [
     reposts: 1,
     comments: [
       {
-        id: 1,
+        id: 5,
         authorID: 1,
         time: "2017-09-20T20:32:25.884Z",
         content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         attacments: '',
-        likes: 12
+        likes: 1
       }
     ]
   }
@@ -91,23 +91,29 @@ const actions = {
   },
   addComment( { commit } , payload ) {
 		commit( 'ADD_COMMENT' , payload )
+  },
+  likeComment( { commit } , payload ) {
+    commit( 'LIKE_COMMENT' , payload )
   }
 };
 
 const mutations = {
   LIKE_WALL_POST( state , payload ) {
     state.find( item => {
-			if ( item.id === payload[0] ) {
-				item.likes += payload[1];
-			}
+			if ( item.id === payload[0] ) item.likes += payload[1];
 		})
   },
   ADD_COMMENT( state , payload ) {
     state.find( item => {
-			if ( item.id === payload[0] ) {
-				item.comments.push(payload[1])
-			}
+			if ( item.id === payload[0] ) item.comments.push(payload[1]);
 		})
+  },
+  LIKE_COMMENT( state , payload ) {
+    state.forEach( wallPost => {
+      wallPost.comments.find( commentItem => {
+        if ( commentItem.id === payload[0] ) commentItem.likes += payload[1];
+      })
+    })
   }
 };
 

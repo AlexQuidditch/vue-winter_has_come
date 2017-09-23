0ca119b0c3f8f10caf5412226f59f1fc7b96294e
aleksey.zabaykalsky <aleksey.zabaykalskiy@gotechsoftware.com>
2017-09-23 23:17:45 +0300

    added simple ID generator

---
+++ src/helpers/randomGenerators.js
@@ -0,0 +1,4 @@
+const getRandomInt = ( min , max ) => Math.floor(Math.random() * (max - min)) + min;
+// Возвращает случайное целое число между min (включительно) и max (не включая max)
+
+export { getRandomInt };

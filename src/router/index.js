8e6b5c6087bd2149afe42dc25b36dacef6cd6cca
aleksey.zabaykalsky <aleksey.zabaykalskiy@gotechsoftware.com>
2017-09-23 03:45:47 +0300

    added scrollBehavior

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 Vue.use(Router);
 
+import { scrollBehavior } from './options.js';
+
 import routes from './routes.js';
 
 export default new Router({
-	mode: 'history',
-	base: __dirname,
-	linkExactActiveClass: '_exact-active',
-	linkActiveClass: '_active',
-	routes
+  mode: 'history',
+  base: __dirname,
+  scrollBehavior,
+  linkExactActiveClass: '_exact-active',
+  linkActiveClass: '_active',
+  routes
 });

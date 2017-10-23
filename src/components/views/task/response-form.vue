df043b9009824c8f75987da77e7f1f07b05999e0
AlexQuidditch <alexquidditch@gmail.com>
2017-10-23 23:09:23 +0300

    creating response form

---
+++ src/components/views/task/response-form.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <form @submit.prevent="sendResponse()"
+        class="response-form">
+    <header class="response-form-header">
+      header
+    </header>
+    <div class="response-form-content"></div>
+    <footer class="response-form-footer"></footer>
+  </form>
+</template>
+
+<script>
+
+  export default {
+    name: "Response-Form",
+    data: () => ({
+
+    })
+  };
+
+</script>
+
+<style lang="scss">
+
+  @import "../../../stylesheets/partials/_mixins.scss";
+
+  .response-form {
+    margin-top: 20px;
+    padding: 20px;
+    color: #4a4a4a;
+    color: var(--charcoal-grey);
+    background-color: #fff;
+    background-color: var(--whited);
+    @include MDShadow-1;
+  }
+
+</style>

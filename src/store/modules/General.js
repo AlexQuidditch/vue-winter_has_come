c82f00d1c545a294acc5f6e9f7b6c0d2e2b921c2
AlexQuidditch <alexquidditch@gmail.com>
2017-10-26 10:21:03 +0300

    prod deploy

--- src/store/modules/General.js
+++ src/store/modules/General.js
@@ -1,6 +1,6 @@
 //Imports on top
 
-// const state = 'http://185.185.69.137:8080';
-const state = 'http://localhost:8080';
+const state = 'http://185.185.69.137:8080';
+// const state = 'http://localhost:8080';
 
 export default { state };

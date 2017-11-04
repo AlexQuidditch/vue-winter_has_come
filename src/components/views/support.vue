2b9abff37f4322b23fecd3dd3282867888aa3a8e
AlexQuidditch <alexquidditch@gmail.com>
2017-11-04 04:53:57 +0300

    codestyle

--- src/components/views/support.vue
+++ src/components/views/support.vue
@@ -1,32 +1,32 @@
 <template lang="html">
-    <main class="main _support">
-		<navigation-panel></navigation-panel>
-		<transition name="fade" mode="out-in">
-			<component :is=" $route.query.section || 'opened' "></component>
-		</transition>
-    </main>
+  <main class="main _support">
+    <navigation-panel></navigation-panel>
+    <transition name="fade" mode="out-in">
+      <component :is=" $route.query.section || 'opened' "></component>
+    </transition>
+  </main>
 </template>
 
 <script>
 
-	import navigationPanel from './support/navigation-panel.vue';
-	import opened from './support/opened.vue';
-	import closed from './support/closed.vue';
-	import newReq from './support/new.vue';
+  import navigationPanel from './support/navigation-panel.vue';
+  import opened from './support/opened.vue';
+  import closed from './support/closed.vue';
+  import newReq from './support/new.vue';
 
-    export default {
-        name: "support",
-		components: { opened , closed , newReq , navigationPanel }
-    };
+  export default {
+    name: "support",
+    components: { opened , closed , newReq , navigationPanel }
+  };
 
 </script>
 
 <style lang="scss">
 
-	@import "../../stylesheets/partials/_mixins";
+  @import "../../stylesheets/partials/_mixins";
 
-    .main._support {
-    	padding: 30px;
-    }
+  .main._support {
+    padding: 30px;
+  }
 
 </style>
